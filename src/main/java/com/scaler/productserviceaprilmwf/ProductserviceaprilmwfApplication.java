7399790dc7152f702bfed357d772934e249c67f7
Avinash <avinashgudipati123@gmail.com>
2025-04-16 15:27:04 +0530

    myFirstCommit

---
+++ src/main/java/com/scaler/productserviceaprilmwf/ProductserviceaprilmwfApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.productserviceaprilmwf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductserviceaprilmwfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductserviceaprilmwfApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/scaler/productserviceaprilmwf/controllers/HelloController.java
@@ -0,0 +1,25 @@
+package com.scaler.productserviceaprilmwf.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("hello")
+public class HelloController {
+
+        @GetMapping("/say/{name}/{times}")
+        public String sayHello(@PathVariable("name") String requesterName, @PathVariable("times") int numberOfTimes) {
+
+            String answer = "";
+            for (int i = 0; i < numberOfTimes; i++) {
+
+                answer += "Hello " + requesterName;
+                answer += "<br>";
+            }
+
+            return answer;
+        }
+}
